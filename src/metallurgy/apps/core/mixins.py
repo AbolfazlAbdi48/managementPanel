e1025176ba73212fbc34e20a706faceb4bde0213
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-01-24 23:57:42 +0330

    add IsSuperUserOrDepartmentStaffUser mixin & update DepartmentListView queryset

--- src/metallurgy/apps/core/mixins.py
+++ src/metallurgy/apps/core/mixins.py
@@ -1,4 +1,6 @@
 from django.http import Http404
+from django.shortcuts import get_object_or_404
+from ..departments.models import Department
 
 
 class IsSuperUserMixin:
@@ -6,5 +8,19 @@ class IsSuperUserMixin:
 
     def dispatch(self, request, *args, **kwargs):
         if request.user and request.user.is_superuser:
+            return super().dispatch(request, *args, **kwargs)
+        raise Http404
+
+
+class IsSuperUserOrDepartmentStaffUserMixin:
+    """
+    Check user is superuser or department staffuser.
+    """
+
+    def dispatch(self, request, pk, name, *args, **kwargs):
+        department = get_object_or_404(Department, pk=pk)
+        if request.user.is_authenticated \
+                and request.user in department.staff_users.all() \
+                or request.user.is_superuser:
             return super().dispatch(request, *args, **kwargs)
         raise Http404
--- src/metallurgy/apps/departments/views.py
+++ src/metallurgy/apps/departments/views.py
@@ -1,24 +1,33 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Department
 from .forms import CreateUpdateDepartmentForm
-from ..core.mixins import IsSuperUserMixin
+from ..core.mixins import IsSuperUserMixin, IsSuperUserOrDepartmentStaffUserMixin
 
 
 # Create your views here.
-class DepartmentsListView(IsSuperUserMixin, ListView):
+class DepartmentsListView(ListView):
     """
     The view return all departments,
     only superuser can see this view.
     """
 
-    model = Department
-    ordering = ['-id']
+    def get_queryset(self):
+        current_user = self.request.user
+        if current_user.is_superuser:
+            return Department.objects.all().order_by('-id')
+        elif current_user.is_staff:
+            return Department.objects.filter(staff_users__in=[current_user]).order_by('-id')
+        else:
+            raise Http404
+
     template_name = 'departments/departments_list.html'
     paginate_by = 12
 
 
-class DepartmentDetailView(DetailView):
+class DepartmentDetailView(IsSuperUserOrDepartmentStaffUserMixin, DetailView):
     """
     The view return department by pk,
     superuser and staff can see this view.
@@ -26,7 +35,8 @@ class DepartmentDetailView(DetailView):
 
     def get_object(self, queryset=None):
         department_pk = self.kwargs.get('pk')
-        return Department.objects.filter(pk=department_pk).first()
+        department = get_object_or_404(Department, pk=department_pk)
+        return department
 
     template_name = 'departments/department_detail.html'
     context_object_name = 'department'
@@ -44,10 +54,11 @@ class DepartmentCreateView(IsSuperUserMixin, CreateView):
     form_class = CreateUpdateDepartmentForm
 
 
-class DepartmentUpdateView(UpdateView):
+class DepartmentUpdateView(IsSuperUserOrDepartmentStaffUserMixin, UpdateView):
     def get_object(self, queryset=None):
         department_pk = self.kwargs.get('pk')
-        return Department.objects.filter(pk=department_pk).first()
+        department = get_object_or_404(Department, pk=department_pk)
+        return department
 
     template_name = 'departments/department_create_update.html'
     success_url = reverse_lazy('departments:list')
