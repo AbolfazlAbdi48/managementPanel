99c5c14f927f6ab1a9512fe1321a9331b90dfe80
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-02-22 18:16:48 +0330

    General update

--- src/metallurgy/apps/departments/forms.py
+++ src/metallurgy/apps/departments/forms.py
@@ -2,14 +2,15 @@ from django import forms
 from ..users.models import User
 from .models import Department
 
-staff_users = User.objects.filter(is_staff=True, is_superuser=False)
-STAFF_USERS_CHOICES = tuple(
-    [
-        (user.id, user.username) for user in staff_users
-    ]
-)
 
 class CreateUpdateDepartmentForm(forms.ModelForm):
+    staff_users = User.objects.filter(is_staff=True, is_superuser=False)
+    STAFF_USERS_CHOICES = tuple(
+        [
+            (user.id, user.username) for user in staff_users
+        ]
+    )
+
     staff_users = forms.MultipleChoiceField(
         choices=STAFF_USERS_CHOICES,
         widget=forms.SelectMultiple(
--- src/metallurgy/envs/common.py
+++ src/metallurgy/envs/common.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ('/src/assets',)
+STATICFILES_DIRS = ('/assets',)
 STATIC_ROOT = '/src/static'
 
 MEDIA_URL = 'media/'
